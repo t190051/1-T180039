76344e461a106f292f3c8ddb554b2e0c4650d55c
T180039 <t180039@mail.ryukoku.ac.jp>
2020-05-28 14:37:08 +0000

    提出

--- kadai1-1.c
+++ kadai1-1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int f(int x) { return 0; }
+int f(int x) { return x*x; }
 
 int main() {
     int x, y;
 
     for(x = -8; x < 9 ; x++) {
-        for(y = 0; y < x*x; y++) {
+        for(y = 0; y < f(x); y++) {
             printf("*");
         }
         printf("\n");
